c987413c092f0a1e277d0ef4f27e64477adda592
ChilenoSika <jaum97@yahoo.com.br>
2019-03-21 23:29:01 -0300

    Hooks are cool

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -1,7 +1,24 @@
 import React, { useState } from "react";
 
 export default function App() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState([
+    { id: 1, name: "repo-teste" },
+    { id: 2, name: "repo-teste" },
+    { id: 3, name: "repo-teste" }
+  ]);
 
-  return <h1>hello world!</h1>;
+  function handleAddRepository() {
+    setRepositories([...repositories, { id: Math.random, name: "new-repo" }]);
+  }
+
+  return (
+    <>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
+      <button onClick={handleAddRepository}>Add repository</button>
+    </>
+  );
 }
