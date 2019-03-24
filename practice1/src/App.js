e56e4843b261c871d1e097e32b2f4075c2536da2
ChilenoSika <jaum97@yahoo.com.br>
2019-03-23 23:31:55 -0300

    Testing useEffect

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -1,24 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const [repositories, setRepositories] = useState([
-    { id: 1, name: "repo-teste" },
-    { id: 2, name: "repo-teste" },
-    { id: 3, name: "repo-teste" }
-  ]);
+  const [repositories, setRepositories] = useState([]);
 
+  /*
   function handleAddRepository() {
-    setRepositories([...repositories, { id: Math.random, name: "new-repo" }]);
+    setRepositories([
+      ...repositories, 
+      { id: Math.random, name: "new-repo" }
+    ]);
   }
+  */
+
+  useEffect(async () => {
+    const responde = await fetch("https://api.github.com/users/jaum97/repos");
+    const data = await responde.json();
+
+    setRepositories(data);
+  }, []);
 
   return (
-    <>
-      <ul>
-        {repositories.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
-      <button onClick={handleAddRepository}>Add repository</button>
-    </>
+    <ul>
+      {repositories.map(repo => (
+        <li key={repo.id}>{repo.name}</li>
+      ))}
+    </ul>
   );
 }
