7cf32bbdf6e4b7a818939a688559f56e9a40de6b
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-08-20 08:49:13 -0300

    Update

--- practice5/src/helpers/customHooks.ts
+++ practice5/src/helpers/customHooks.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 
 export const useLogger = <T>(logged: T): void => {
-  const log = (): void => console.log({[ logged ]: logged})
+  const log = (): void => console.log(logged)
   useEffect(log, [ logged ])
 }
 
@@ -13,4 +13,27 @@ export const useToggle = (initialState: boolean): [ boolean, () => void ] => {
   }
 
   return [ state, toggleState ]
+}
+
+interface IListItem<T> {
+  label: string
+  value: T
+  selected: boolean
+}
+
+/* needs testing */
+type ReturnType<T> = [Array<IListItem<T>>, (index: number) => void]
+
+export function useRadioList<T>( initialState: Array<IListItem<T>> ): ReturnType<T> {
+  const [ list, setList ] = useState(initialState)
+
+  const selectItem = (index: number): void => {
+    const newList = list.map((item, i) => 
+      i === index 
+        ? {...item, selected: true} 
+        : {...item, selected: false})
+        setList(newList)
+  }
+
+  return [ list, selectItem ]
 }
